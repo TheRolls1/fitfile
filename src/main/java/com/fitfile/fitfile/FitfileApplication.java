7229a008193cd257424741cb05f176222ba7bb80
TheRolls1 <ma.palavecinos@duocuc.cl>
2025-05-28 18:52:48 -0400

    quinto commit

---
+++ src/main/java/com/fitfile/fitfile/FitfileApplication.java
@@ -0,0 +1,11 @@
+package com.fitfile.fitfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitfileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FitfileApplication.class, args);
+    }
+}
---
+++ src/test/java/com/fitfile/fitfile/FitfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitfile.fitfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
